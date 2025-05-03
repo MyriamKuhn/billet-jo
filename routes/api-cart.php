7580ac85cb342212c92b8b3b132ca99f54902ce6
MyriamKuhn <myriam.kuehn@free.fr>
2025-05-03 15:45:14 +0200

    Prepare the routes for the API Cart

--- routes/api-cart.php
+++ routes/api-cart.php
@@ -1,6 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CartController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckOriginMiddleware;
 
-Route::get('/cart/ping', [CartController::class, 'ping']);
+Route::prefix('cart')->group(function () {
+    // This route is used to get all products in the cart.
+    Route::get('/', [CartController::class, 'index'])
+        ->name('cart.index');
+
+    // This route is used to add a product to the cart.
+    Route::post('/', [CartController::class, 'store'])
+        ->name('cart.store')
+        ->middleware('auth:sanctum');
+
+    // This route is used to remove a product from the cart.
+    Route::delete('/{product}', [CartController::class, 'destroy'])
+        ->name('cart.destroy')
+        ->middleware('auth:sanctum');
+})->middleware([CheckOriginMiddleware::class]);
